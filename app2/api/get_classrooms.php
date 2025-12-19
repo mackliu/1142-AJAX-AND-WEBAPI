cc16ebbcf61b1faa1170a2d232c995e3b5a1ab67
mackliu <macklun@gmail.com>
2025-12-19 11:47:37 +0800

    美化班級按鈕

--- app2/api/get_classrooms.php
+++ app2/api/get_classrooms.php
@@ -1,10 +1,16 @@
 <?php 
 include_once "DB.php";
-
+$words=[1=>'一',2=>'二',3=>'三',4=>'四',5=>'五',6=>'六',7=>'七',8=>'八',9=>'九',10=>'十'];
 $Student=new DB('students');
 $rows=$Student->all(" group by `classroom` ");
 $temp=[];
+
 foreach($rows as $row){
-    $temp[]=$row['classroom'];
+    $year=(int)mb_substr($row['classroom'],0,1,'utf-8');
+    $room=(int)mb_substr($row['classroom'],1,2,'utf-8');
+
+    $temp[$row['classroom']]=$words[$year]."年".$words[$room]."班";
 }
+
 echo json_encode($temp);   
+
